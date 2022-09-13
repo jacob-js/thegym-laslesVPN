4653dc166157401927de8caeb9f1655ac4554cef
Jacob Dev <mercihabam@gmail.com>
2022-09-13 15:56:10 +0200

    initializing the repo

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*.html'],
+  theme: {
+    extend: {
+      colors: {
+        default: '#F53838'
+      }
+    },
+  },
+  plugins: [],
+}
